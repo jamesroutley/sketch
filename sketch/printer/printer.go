d5219106e15efec2c847bd39e9654e95263da325
James Routley <jroutley@gmail.com>
2021-01-14 00:56:44 +0000

    Group import expressions when formatting

--- sketch/printer/printer.go
+++ sketch/printer/printer.go
@@ -43,13 +43,43 @@ func PrettyPrintTopLevelDo(ast types.SketchType) string {
 	// separating each with a newline.
 	var b bytes.Buffer
 
-	for _, expr := range list.Items[1:] {
+	items := list.Items[1:]
+
+	for i, expr := range items {
 		fmt.Fprintln(&b, PrettyPrint(expr))
+
 		// Comments above top level statements should 'stick' to them
-		if expr.Type() != "comment" {
-			fmt.Fprint(&b, "\n")
+		if expr.Type() == "comment" {
+			continue
 		}
+
+		// Imports should be on consecutive lines. We check if the next item is
+		// an import rather than this one so the final import has a newline
+		// below it
+		if i+1 < len(items) && isImportExpression(items[i+1]) {
+			continue
+		}
+
+		fmt.Fprint(&b, "\n")
 	}
 
 	return strings.TrimSpace(b.String())
+}
+
+func isImportExpression(expr types.SketchType) bool {
+	list, ok := expr.(*types.SketchList)
+	if !ok {
+		return false
+	}
+	items := list.Items
+	if len(items) == 0 {
+		return false
+	}
+
+	operator, ok := items[0].(*types.SketchSymbol)
+	if !ok {
+		return false
+	}
+
+	return operator.Value == "import"
 }
